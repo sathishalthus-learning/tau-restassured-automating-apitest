c92a6ec66c02412afb67ac29c99e3a6642bd6bc5
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-01-19 13:41:38 +0530

    Course Finished.

--- src/test/java/rest/zippopotam/L6_Serialization.java
+++ src/test/java/rest/zippopotam/L6_Serialization.java
@@ -1,5 +1,7 @@
 package rest.zippopotam;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,6 +10,7 @@ import com.tngtech.java.junit.dataprovider.DataProvider;
 import com.tngtech.java.junit.dataprovider.DataProviderRunner;
 import com.tngtech.java.junit.dataprovider.UseDataProvider;
 
+import entities.Location;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
@@ -25,18 +28,33 @@ public class L6_Serialization {
 	
 	// http://api.zippopotam.us/us
 	
-	
 	@Test
-	public void requestResponseAndExtract() {
-		given()
-		.when()
-			.get("http://localhost:9876/us/90210")
-		.then()
-			.assertThat()
-			.body("response.places.place.placename", equalTo("Beverly Hills"))
-//			.body("response.places.place[2].placename", equalTo("Beverly Hills"))
-//			.body("response.places.place[2].@attribute", equalTo("Beverly Hills"))
-		;
+    public void serialzation() {
+
+        Location location =
+        given().
+        when().
+            get("http://api.zippopotam.us/us/90210").
+        as(Location.class);
+        
+        assertEquals("Beverly Hills",location.getPlaces().get(0).getPlaceName())
+        ;
 			}
 	
+	@Test
+    public void deSerialzation() {
+
+        Location location = new Location();
+        location.setCountry("Netherlands");
+
+        given().
+            contentType(ContentType.JSON).
+            body(location).
+            log().body().
+        when().
+            post("http://localhost:9876/lv/1050").
+        then().
+            assertThat().
+            statusCode(200);
+			}
 }
